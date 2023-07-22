686a570e186e1130b16d1513d5ad472d5437c770
feliciasanm <47961812+feliciasanm@users.noreply.github.com>
2023-07-22 23:10:51 +0700

    Do a light refactoring on timestamp prefix for Google Cloud Storage objects

--- dags/bpi_etl_bigquery.py
+++ dags/bpi_etl_bigquery.py
@@ -5,7 +5,24 @@ import pendulum
 # We will write the DAG using the TaskFlow API introduced in Airflow 2.0
 from airflow.decorators import dag, task
 
-
+def create_random_dt_prefix(timestamp, filename):
+    """
+    Create a random prefix using MD5 hash on top of a datetime-based prefix for Google Cloud Storage objects
+    
+    A random prefix when using GCS is great to help GCP autoscale our requests
+    See more here: https://cloud.google.com/storage/docs/request-rate#naming-convention0
+    """
+    
+    import hashlib
+    
+    with open(filename, 'rb') as file:
+        md5_prefix = hashlib.md5(file.read()).hexdigest()[0:6]
+            
+    timestamp_prefix = timestamp.strftime('%Y-%m-%d-%H-%M-%S')
+    
+    return f'{md5_prefix}-{timestamp_prefix}'
+        
+    
 @dag(
     schedule = '@hourly',
     start_date = pendulum.datetime(2022, 12, 18, tz = 'Asia/Jakarta'),
@@ -24,7 +41,7 @@ def bpi_etl_bigquery():
         Extract Bitcoin Price Index containing conversion rates of Bitcoin with USD, GBP, and EUR from [CoinDesk's API](https://api.coindesk.com/v1/bpi/currentprice.json), and save the raw JSON on Google Cloud Storage.
         """
         
-        import requests, json, hashlib
+        import requests, json
         from google.cloud import storage as gcp_storage
         
         # We will store several details that the next tasks will need
@@ -49,13 +66,7 @@ def bpi_etl_bigquery():
         
         # Upload our saved JSON to Google Cloud Storage (snapshotting raw data)
         
-        # A random prefix when using GCS is great to help GCP autoscale our requests
-        # See https://cloud.google.com/storage/docs/request-rate#naming-convention0
-        with open(return_dict['extract_file'], 'rb') as file:
-            md5_prefix = hashlib.md5(file.read()).hexdigest()[0:6]
-            
-        timestamp_prefix = run_timestamp.strftime('%Y-%m-%d-%H-%M-%S')
-        complete_prefix = f'{md5_prefix}-{timestamp_prefix}'
+        complete_prefix = create_random_dt_prefix(run_timestamp, return_dict['extract_file'])
         
         # Must be stored to ensure the next tasks know where to find it!
         return_dict['gcs_dest'] = f"data/raw/{complete_prefix}/{return_dict['extract_file']}"
@@ -80,7 +91,7 @@ def bpi_etl_bigquery():
         
         from airflow.models import Variable
         
-        import requests, json, hashlib
+        import requests, json
         from google.cloud import storage as gcp_storage
         
         fetch_datetime = pendulum.parse(fetch_timestamp)
@@ -113,13 +124,7 @@ def bpi_etl_bigquery():
         
         # Upload our saved JSON to Google Cloud Storage (snapshotting raw data)
         
-        # A random prefix when using GCS is great to help GCP autoscale our requests
-        # See https://cloud.google.com/storage/docs/request-rate#naming-convention0
-        with open(return_dict['extract_file'], 'rb') as file:
-            md5_prefix = hashlib.md5(file.read()).hexdigest()[0:6]
-            
-        timestamp_prefix = pendulum.now().strftime('%Y-%m-%d-%H-%M-%S')
-        complete_prefix = f'{md5_prefix}-{timestamp_prefix}'
+        complete_prefix = create_random_dt_prefix(pendulum.now(), return_dict['extract_file'])
         
         # Must be stored to ensure the next tasks know where to find it!
         return_dict['gcs_dest'] = f"data/raw/{complete_prefix}/{return_dict['extract_file']}"
@@ -141,7 +146,7 @@ def bpi_etl_bigquery():
         Clean, transform, and enrich BPI data with additional IDR conversion rate, given input of all the raw data necessary, and save as a Parquet file on Google Cloud Storage, prepared for loading into BigQuery later 
         """
         
-        import json, hashlib
+        import json
         import pandas as pd
         
         from google.cloud import storage as gcp_storage
@@ -219,17 +224,11 @@ def bpi_etl_bigquery():
         bpi_df.to_parquet(return_dict['extract_file'], index = False, engine = 'fastparquet', has_nulls = ['disclaimer', 'chart_name'])
         
         
-        # Upload our saved parquet to Google Cloud Storage (snapshotting raw data)
+        # Upload our saved parquet to Google Cloud Storage (snapshotting transformed data)
         
-        # A random prefix when using GCS is great to help GCP autoscale our requests
-        # See https://cloud.google.com/storage/docs/request-rate#naming-convention0
-        with open(return_dict['extract_file'], 'rb') as file:
-            md5_prefix = hashlib.md5(file.read()).hexdigest()[0:6]
-            
-        timestamp_prefix = pendulum.now().strftime('%Y-%m-%d-%H-%M-%S')
-        complete_prefix = f'{md5_prefix}-{timestamp_prefix}'
+        complete_prefix = create_random_dt_prefix(pendulum.now(), return_dict['extract_file'])
         
-        # Must be stored to ensure the next tasks know where to find it!
+        # Must be stored to ensure the next task know where to find it!
         return_dict['gcs_dest'] = f"data/bigquery_load/{complete_prefix}/{return_dict['extract_file']}"
         return_dict['gcs_bucket'] = '371516-bpi-etl'
         
